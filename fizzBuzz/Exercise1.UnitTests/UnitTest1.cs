e6712485c8cece4159fd6e771107e44058c611e8
amandachau36 <amandachau36@gmail.com>
2020-02-10 09:28:10 +1100

    unit tests

---
+++ fizzBuzz/Exercise1.UnitTests/UnitTest1.cs
@@ -0,0 +1,77 @@
+using NUnit.Framework;
+
+namespace Exercise1.UnitTests
+{
+    public class FizzTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FizzBuzz_NumberIs3_ReturnsFizz()
+        {
+            //Arrange
+            var fizz = new Fizz();
+            
+            //Act
+            var result = fizz.FizzBuzz(3);
+
+            //Assert
+            StringAssert.AreEqualIgnoringCase("Fizz", result);
+        }
+        
+        [Test]
+        public void FizzBuzz_NumberIs5_ReturnsBuzz()
+        {
+            //Arrange
+            var fizz = new Fizz();
+            
+            //Act
+            var result = fizz.FizzBuzz(5);
+
+            //Assert
+            StringAssert.AreEqualIgnoringCase("Buzz", result);
+        }
+        
+        [Test]
+        public void FizzBuzz_NumberIs15_ReturnsFizzBuzz()
+        {
+            //Arrange
+            var fizz = new Fizz();
+            
+            //Act
+            var result = fizz.FizzBuzz(15);
+
+            //Assert
+            StringAssert.AreEqualIgnoringCase("FizzBuzz", result);
+        }
+
+        [Test]
+        public void FizzBuzz_NumberIs1_Returns1()
+        {
+            //Arrange
+            var fizz = new Fizz();
+            
+            //Act
+            var result = fizz.FizzBuzz(1);
+            
+            //Assert 
+            StringAssert.AreEqualIgnoringCase("1", result);
+        }
+    }
+}
+
+//Fizz Buzz Kata
+//Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
+
+//Example Output
+//1
+//2
+//Fizz
+//4
+//Buzz
+//Fizz
+//7
+//etc.
---
+++ fizzBuzz/Exercise1/Fizz.cs
@@ -0,0 +1,7 @@
+namespace fizzBuzz
+{
+    public class Fizz
+    {
+        
+    }
+}
