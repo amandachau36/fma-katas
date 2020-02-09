da05bfa0fb7f19a4bb30cb1f89e3ebe468995a95
amandachau36 <amandachau36@gmail.com>
2020-02-10 09:29:16 +1100

    fizzBuzz solution

--- fizzBuzz/Exercise1/Fizz.cs
+++ fizzBuzz/Exercise1/Fizz.cs
@@ -1,7 +1,34 @@
-namespace fizzBuzz
+
+using System;
+using System.Text;
+
+namespace Exercise1
 {
     public class Fizz
     {
-        
+        public string FizzBuzz(int number)
+        {   
+            var stringBuilder = new StringBuilder();
+            
+
+            if (number % 3 == 0)
+                stringBuilder.Append("Fizz");
+
+            if (number % 5 == 0)
+                stringBuilder.Append("Buzz");
+
+            if (stringBuilder.Length == 0)
+                stringBuilder.Append(number);
+            
+            return stringBuilder.ToString();
+        }
+
+        public void PrintFizzBuzz()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+        }
     }
 }
---
+++ fizzBuzz/Exercise1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var fizz = new Fizz();
+            
+            fizz.PrintFizzBuzz();
+
+
+        }
+    }
+}
+
+
+
+// automated testing - the practice of writing code to test our code 
+// and then run those tests in an automated fashion
+
+
+// code = production code  + test code
+
+// Benefits of automated testing - test your code frequently and in less time,
+// find bugs before deployment (reduce but not eliminate)
+// deploy with confidence
+// also allows for refactoring confidently
+// focus more on quality 
+
+// Unit test - tests a unit of an application without its **external dependencies** 
+// can be one or multiple classes 
+// + Cheap to write and execute fast
+// - don't give you a lot of confidence
+
+// Integration test
+// tests the applications with its **external dependencies**
+// + give you more confidence
+// - takes longer to execute
+
+// End-to-end tests 
+// Drives an application through its UI 
+// EX. Selenium
+// + give you the greatest confidence 
+// - very SLOW, very BRITTLE
+
+// Test pyramid
+// % unit (include edge cases) > integration > E2E (not for edge cases)
+// unit test - test complex logic
+// integration test - interaction with the database 
+
+//takeaways 
+//Favour unit tests to e2e test
+//Cover unit test gaps with integration tests
+//Use end-to-end tests sparingly
+
+//testing frameworks = Library + Test Runner
+//NUnit
+//MSTest (built into VS)
+//XUnit
+
+
+
+//option F5 to run
+
+
+
