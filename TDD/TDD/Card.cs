257a93ed61e7c03d2e48a58d6bb0e7da1b4e9939
amandachau36 <amandachau36@gmail.com>
2020-03-06 09:07:47 +1100

    first session with sam

--- TDD/TDD/Card.cs
+++ TDD/TDD/Card.cs
@@ -1,7 +1,23 @@
+using System;
+
 namespace TDD
 {
     public class Card
     {
+        public decimal Total { get; private set; }
+        
+        public TapOn _TapOn { get; private set; }
+
+        public void TopUp(decimal amount)
+        {
+            Total += amount;
+        }
+
+        public void TapOn(TapOn tapOn)
+        {
+            _TapOn = tapOn;
+        }
+        
         
     }
 }
--- TDD/TDD/TapOn.cs
+++ TDD/TDD/TapOn.cs
@@ -1,7 +1,19 @@
+using System;
+
 namespace TDD
 {
     public class TapOn
     {
-        
+        public readonly DateTime Time;
+
+        public readonly Guid MachineId;
+
+        public TapOn(Guid machineId,  DateTime time )
+        {
+            Time = time;
+
+            MachineId = machineId;
+        }
+
     }
 }
