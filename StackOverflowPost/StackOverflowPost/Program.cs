94642d4b1ebf51ce12e2b744d01a31fd6d950770
amandachau36 <amandachau36@gmail.com>
2020-03-11 18:52:02 +1100

    solution and tests for stack overflow post

---
+++ StackOverflowPost/StackOverflowPost/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post( "C# question", "Blah blah blah" );
+            
+            //post.CurrentVotes = 5;  is not accessible. This creates safety
+            
+            
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            
+            post.DownVote();
+            post.DownVote();
+
+            Console.WriteLine(post.CurrentVotes);
+          
+            
+        }
+    }
+}
