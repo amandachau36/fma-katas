e181fba6f87937190b443822f417e2687f6c48b2
Amanda <amandachau36@gmail.com>
2020-04-22 17:35:57 +1000

    Component test passing

--- ConferenceTrackManagementKata/ConferenceTrack.UnitTests/ComponentTests/ComponentTests.cs
+++ ConferenceTrackManagementKata/ConferenceTrack.UnitTests/ComponentTests/ComponentTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using ConferenceTrack.Business;
 using ConferenceTrack.Client;
 using ConferenceTrack.Client.Display;
@@ -33,9 +34,39 @@ namespace ConferenceTrack.UnitTests.ComponentTests
             conferenceTrackManager.ManageTracks();
              
             //Assert
-            Assert.Empty(consoleDisplay.Messages); 
-             
-            //acceptance tests
+            var expectedMessages = new List<string>
+            {
+                Constants.Welcome,
+                Constants.FilePathPrompt,
+                "Track 1",
+                "09:00  Writing Fast Tests Against Enterprise Rails 60min",
+                "10:00  Communicating Over Distance 60min",
+                "11:00  Rails Magic 60min",
+                "12:00  Lunch",
+                "01:00  Ruby Errors from Mismatched Gem Versions 45min",
+                "01:45  Common Ruby Errors 45min",
+                "02:30  Accounting-Driven Development 45min",
+                "03:15  Pair Programming vs Noise 45min",
+                "04:00  Clojure Ate Scala (on my project) 45min",
+                "05:00  Networking Event",
+                "Track 2",
+                "09:00  Ruby on Rails: Why We Should Move On 60min",
+                "10:00  Ruby on Rails Legacy App Maintenance 60min",
+                "11:00  Overdoing it in Python 45min",
+                "11:45  Rails for Python Developers lightning",
+                "12:00  Lunch",
+                "01:00  Lua for the Masses 30min",
+                "01:30  Woah 30min",
+                "02:00  Sit Down and Write 30min",
+                "02:30  Programming in the Boondocks of Seattle 30min",
+                "03:00  Ruby vs. Clojure for Back-End Development 30min",
+                "03:30  A World Without HackerNews 30min",
+                "04:00  User Interface CSS in Rails Apps 30min",
+                "05:00  Networking Event"
+            };
+          
+            Assert.True(consoleDisplay.Messages.SequenceEqual(expectedMessages)); 
+            
         }
         
         public class ConsoleDisplayStub : IDisplay  //Can also do with moq 
@@ -48,21 +79,27 @@ namespace ConferenceTrack.UnitTests.ComponentTests
 
             public List<Track> PrepareDisplay(List<Track> tracks)
             {
-                throw new NotImplementedException();
+                foreach (var talk in tracks.SelectMany(t => t.Talks))
+                {
+                    talk.SetScheduledTalk($"{talk.TalkTime.ToString(Constants.FormatTime)}  {talk.TalkTitle}");
+                }
+
+                return tracks;
             }
 
             public void WriteDisplay(List<Track> tracks)
             {
-                throw new NotImplementedException();
-            }
-
-            public void Display(List<Track> tracks)
-            {
-                foreach (var track in tracks)
+                foreach (var t in tracks)
                 {
-                    
+                    Messages.Add(t.TrackTitle);
+                
+                    foreach (var talk in t.Talks)
+                    {
+                        Messages.Add(talk.ScheduledTalk); 
+                    }
                 }
             }
+            
 
             public void DisplayError(string error)
             {
@@ -73,6 +110,8 @@ namespace ConferenceTrack.UnitTests.ComponentTests
             {
                 return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "OriginalTestInput.txt");
             }
+            
+      
         }
     }
     
